066900a7ff96e3f74c2b3bb2a72cc3790c790a0c
kruthik693 <kruthik693@gmail.com>
2026-03-09 20:17:54 +0530

    IN PROCESS

--- src/main/java/com/microservice/app/controller/RegisterController.java
+++ src/main/java/com/microservice/app/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package com.microservice.app.controller;
 import com.microservice.app.dto.RegisterDto;
 import com.microservice.app.service.RegisterService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
